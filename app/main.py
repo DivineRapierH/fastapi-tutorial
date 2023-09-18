838b214f3eb864434d5a00d42efa986637aed623
Lifan Huang <lifan_huang@outlook.com>
2023-09-18 19:03:05 +0800

    add items api

--- app/main.py
+++ app/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Query
 
 app = FastAPI()
 
@@ -11,3 +11,22 @@ async def root():
 @app.get("/hello/{name}")
 async def say_hello(name: str):
     return {"message": f"Hello {name}"}
+
+
+@app.get("/items")
+async def read_items(page: int = Query(1, gt=0), limit: int = Query(10, gt=0)):
+    # Fake pagination by slicing a list
+    items = ["item1", "item2", "item3", "item4", "item5", "item6", "item7"]
+
+    start = (page - 1) * limit
+    end = start + limit
+
+    total = 7
+    has_next_page = limit * page < total
+    return {
+        "data": items[start:end],
+        "total": total,
+        "page_no": page,
+        "page_size": limit,
+        "has_next_page": has_next_page
+    }
